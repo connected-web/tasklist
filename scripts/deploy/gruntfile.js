377f984cc0bcd74ee013c5751a50eb8aa1140dd6
John Beech <github@mkv25.net>
2017-02-12 17:26:25 +0000

    Create deployment script to upload to mkv25.net/taskslist

---
+++ scripts/deploy/gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+
+  // ftp host
+  var FTP_HOST = 'ftp.mkv25.net';
+  var FTP_USER_LIVE = 'mkv25-live'
+
+  // files to upload and exclude
+  var FTP_LOCAL_FOLDER = "../../web";
+  var FTP_DEST_FOLDER = "";
+  var FTP_EXCLUSIONS_COMMON = ['.ftp*', '.git*', '.hta*', 'deploy', '*.fdproj', 'tasklist.md', 'readme.md', '.sublime'];
+
+  // auth details for live
+  var FTP_LIVE_AUTH = {
+    host: FTP_HOST,
+    port: 21,
+    authKey: FTP_USER_LIVE
+  };
+
+  // load plugins
+  grunt.loadNpmTasks('grunt-ftp-deploy');
+  grunt.loadNpmTasks('grunt-debug-task');
+
+  function ftpStageConfigFor(path, exclusions) {
+    exclusions = exclusions || [];
+    return {
+      auth: FTP_STAGE_AUTH,
+      src: FTP_LOCAL_FOLDER + path,
+      dest: FTP_DEST_FOLDER + path,
+      exclusions: [].concat(FTP_EXCLUSIONS_COMMON, exclusions),
+      forceVerbose: true
+    };
+  }
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    "ftp-deploy": {
+      "live": {
+        auth: FTP_LIVE_AUTH,
+        src: FTP_LOCAL_FOLDER,
+        dest: FTP_DEST_FOLDER,
+        exclusions: FTP_EXCLUSIONS_COMMON,
+        forceVerbose: true
+      }
+    }
+  });
+
+  // Default task(s).
+  grunt.registerTask('release', ['ftp-deploy:live']);
+}
