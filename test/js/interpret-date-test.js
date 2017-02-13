b96cb66190db9abb1a345017c061b302cc05aee5
John Beech <github@mkv25.net>
2017-02-13 00:12:30 +0000

    Add test framework around interpret-date

---
+++ test/js/interpret-date-test.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const interpretDate = require('../../web/js/interpret-date.js');
+
+const fixedTime = new Date(1486940936 * 1000); // Sun Feb 12 2017 23:08:56 GMT+0000 (GMT Standard Time)
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const week = day * 7;
+
+interpretDate.debug = 'all';
+
+function test(input, expected, description) {
+  it(description || `should match '${input}' as ${expected}`, () => {
+    const actual = interpretDate(fixedTime, input).toUTCString();
+    assert.deepEqual(actual, expected);
+  });
+}
+
+describe('Interpret Date', function() {
+  describe('Day of Week Matchers', function() {
+    test('Next sunday', 'Sun, 19 Feb 2017 08:00:00 GMT', 'should match the next Sunday from a Sunday');
+    test('on Monday', 'Mon, 13 Feb 2017 08:00:00 GMT');
+    test('Tues', 'Tue, 14 Feb 2017 08:00:00 GMT');
+    test('wednesday', 'Wed, 15 Feb 2017 08:00:00 GMT');
+    test('thursda', 'Thu, 16 Feb 2017 08:00:00 GMT');
+    test('Fri', 'Fri, 17 Feb 2017 08:00:00 GMT');
+    test('Satur', 'Sat, 18 Feb 2017 08:00:00 GMT');
+    test('Sun', 'Sun, 19 Feb 2017 08:00:00 GMT');
+  });
+});
--- web/js/interpret-date.js
+++ web/js/interpret-date.js
@@ -1,4 +1,90 @@
-function interpretDate(entryDate, dateString) {
-  entry = new Date(entryDate * 1000);
-  return entry + '' + ' ? + ' + dateString;
+var LOG_TABS = '     ';
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+const WEEK = DAY * 7;
+
+function interpretDate(dateContext, dateString) {
+  var entry = new Date(dateFor(dateString, dateContext));
+  if (interpretDate.debug) {
+    console.log(LOG_TABS, 'Intepreted date', dateContext.toUTCString(), dateString, 'as', entry.toUTCString());
+  }
+  return entry;
+}
+
+var matchers = [{
+  regex: /^(Mon|Tue|Wed|Thu|Fri|Sat|Sun)u?s?n?e?s?r?s?d?a?y?$/i,
+  handler: matchDayOfWeek
+}];
+
+function matchDayOfWeek(token, tokens, context) {
+  var matcher = /^(Mon|Tue|Wed|Thu|Fri|Sat|Sun)u?s?n?e?s?r?s?d?a?y?$/i;
+  var key = token.match(matcher)[1].toLowerCase();
+  var dayOffset = {
+    'sun': 0,
+    'mon': 1,
+    'tue': 2,
+    'wed': 3,
+    'thu': 4,
+    'fri': 5,
+    'sat': 6
+  }[key];
+
+  if (!Number.isInteger(dayOffset)) {
+    throw 'Unrecognised day of the week: ' + dayOffset;
+  }
+
+  dayOffset = (dayOffset - context.getDay() + 7) % 7 || 7;
+  var now = context.getTime();
+  var future = new Date(now + (dayOffset * DAY));
+
+  return {
+    setUTCFullYear: future.getUTCFullYear(),
+    setUTCMonth: future.getUTCMonth(),
+    setUTCDate: future.getUTCDate(),
+    setUTCHours: 8,
+    setUTCMinutes: 0,
+    setUTCSeconds: 0
+  };
+}
+
+function tokenize(inputString) {
+  var plainText = inputString.replace(/[^a-z0-9+:\/\-]+/gi, ' ');
+  var sanitized = plainText.replace(/\s+/gi, ' ');
+  var tokens = sanitized.split(' ');
+  return tokens;
+}
+
+function dateFor(dateString, context) {
+  var tokens = tokenize(dateString);
+  var token;
+
+  var results = [];
+  while (tokens.length > 0) {
+    token = tokens.shift();
+    matchers.forEach(function(matcher) {
+      if (matcher.regex.test(token)) {
+        var result = matcher.handler(token, tokens, context);
+        if (interpretDate.debug) {
+          console.log(LOG_TABS, 'Matched', JSON.stringify(result));
+        }
+        results.push(result);
+      };
+    });
+  }
+
+  var date = new Date(context);
+  results.forEach(function(item) {
+    Object.keys(item).forEach(function(key) {
+      var value = item[key];
+      date[key](value);
+    });
+  });
+
+  return date;
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = interpretDate;
 }
