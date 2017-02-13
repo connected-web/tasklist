eafddf29e0120f017db5eb86657db9d081153b24
John Beech <github@mkv25.net>
2017-02-13 01:22:47 +0000

    Add timeUntil to format display date

--- web/js/interpret-date.js
+++ web/js/interpret-date.js
@@ -1,150 +1,156 @@
-var LOG_TABS = '     ';
-const SECOND = 1000;
-const MINUTE = SECOND * 60;
-const HOUR = MINUTE * 60;
-const DAY = HOUR * 24;
-const WEEK = DAY * 7;
+(function() {
+  var LOG_TABS = '     ';
+  const SECOND = 1000;
+  const MINUTE = SECOND * 60;
+  const HOUR = MINUTE * 60;
+  const DAY = HOUR * 24;
+  const WEEK = DAY * 7;
 
-const applyHours = (hours) => {
-  return {
-    setUTCHours: hours,
-    setUTCMinutes: 0,
-    setUTCSeconds: 0
-  };
-};
-
-function interpretDate(dateContext, dateString) {
-  var entry = new Date(dateFor(dateString, dateContext));
-  if (interpretDate.debug) {
-    console.log(LOG_TABS, 'Intepreted date', dateContext.toUTCString(), dateString, 'as', entry.toUTCString());
-  }
-  return entry;
-}
-
-var matchers = [{
-  regex: /^(Mon|Tue|Wed|Thu|Fri|Sat|Sun)u?s?n?e?s?r?s?d?a?y?$/i,
-  handler: matchDayOfWeek
-}, {
-  regex: /^even?i?n?g?$/i,
-  handler: matchEvening
-}, {
-  regex: /^before$/i,
-  handler: matchBefore
-}, {
-  regex: /^after$/i,
-  handler: matchAfter
-}, {
-  regex: /^morn?i?n?g?$/i,
-  handler: matchMorning
-}];
-
-function matchAfter(token, tokens, context) {
-  const subject = tokens.shift();
-  const subjects = {
-    work: applyHours(17),
-    lunch: applyHours(13),
-    school: applyHours(16),
-    breakfast: applyHours(7)
+  const applyHours = (hours) => {
+    return {
+      setUTCHours: hours,
+      setUTCMinutes: 0,
+      setUTCSeconds: 0
+    };
   };
 
-  return subjects[subject] || {};
-}
-
-function matchBefore(token, tokens, context) {
-  const subject = tokens.shift();
-  const subjects = {
-    work: applyHours(7),
-    lunch: applyHours(11),
-    school: applyHours(7),
-    breakfast: applyHours(6)
-  };
-
-  return subjects[subject] || {};
-}
-
-function matchEvening(token, tokens, context) {
-  return {
-    setUTCHours: 18,
-    setUTCMinutes: 0,
-    setUTCSeconds: 0
-  };
-}
-
-function matchMorning(token, tokens, context) {
-  return {
-    setUTCHours: 8,
-    setUTCMinutes: 0,
-    setUTCSeconds: 0
-  };
-}
-
-function matchDayOfWeek(token, tokens, context) {
-  var matcher = /^(Mon|Tue|Wed|Thu|Fri|Sat|Sun)u?s?n?e?s?r?s?d?a?y?$/i;
-  var key = token.match(matcher)[1].toLowerCase();
-  var dayOffset = {
-    'sun': 0,
-    'mon': 1,
-    'tue': 2,
-    'wed': 3,
-    'thu': 4,
-    'fri': 5,
-    'sat': 6
-  }[key];
-
-  if (!Number.isInteger(dayOffset)) {
-    throw 'Unrecognised day of the week: ' + dayOffset;
+  function interpretDate(dateContext, dateString) {
+    var entry = new Date(dateFor(dateString, dateContext));
+    if (interpretDate.debug) {
+      console.log(LOG_TABS, 'Intepreted date', dateContext.toUTCString(), dateString, 'as', entry.toUTCString());
+    }
+    return entry;
   }
 
-  dayOffset = (dayOffset - context.getDay() + 7) % 7 || 7;
-  var now = context.getTime();
-  var future = new Date(now + (dayOffset * DAY));
+  var matchers = [{
+    regex: /^(Mon|Tue|Wed|Thu|Fri|Sat|Sun)u?s?n?e?s?r?s?d?a?y?$/i,
+    handler: matchDayOfWeek
+  }, {
+    regex: /^even?i?n?g?$/i,
+    handler: matchEvening
+  }, {
+    regex: /^before$/i,
+    handler: matchBefore
+  }, {
+    regex: /^after$/i,
+    handler: matchAfter
+  }, {
+    regex: /^morn?i?n?g?$/i,
+    handler: matchMorning
+  }];
 
-  return {
-    setUTCFullYear: future.getUTCFullYear(),
-    setUTCMonth: future.getUTCMonth(),
-    setUTCDate: future.getUTCDate(),
-    setUTCHours: 8,
-    setUTCMinutes: 0,
-    setUTCSeconds: 0
-  };
-}
+  function matchAfter(token, tokens, context) {
+    const subject = tokens.shift();
+    const subjects = {
+      work: applyHours(17),
+      lunch: applyHours(13),
+      school: applyHours(16),
+      breakfast: applyHours(7)
+    };
 
-function tokenize(inputString) {
-  var plainText = inputString.replace(/[^a-z0-9+:\/\-]+/gi, ' ');
-  var sanitized = plainText.replace(/\s+/gi, ' ');
-  var tokens = sanitized.split(' ');
-  return tokens;
-}
+    return subjects[subject] || {};
+  }
 
-function dateFor(dateString, context) {
-  var tokens = tokenize(dateString);
-  var token;
+  function matchBefore(token, tokens, context) {
+    const subject = tokens.shift();
+    const subjects = {
+      work: applyHours(7),
+      lunch: applyHours(11),
+      school: applyHours(7),
+      breakfast: applyHours(6)
+    };
 
-  var results = [];
-  while (tokens.length > 0) {
-    token = tokens.shift();
-    matchers.forEach(function(matcher) {
-      if (matcher.regex.test(token)) {
-        var result = matcher.handler(token, tokens, context);
-        if (interpretDate.debug) {
-          console.log(LOG_TABS, 'Matched', JSON.stringify(result));
-        }
-        results.push(result);
-      };
+    return subjects[subject] || {};
+  }
+
+  function matchEvening(token, tokens, context) {
+    return {
+      setUTCHours: 18,
+      setUTCMinutes: 0,
+      setUTCSeconds: 0
+    };
+  }
+
+  function matchMorning(token, tokens, context) {
+    return {
+      setUTCHours: 8,
+      setUTCMinutes: 0,
+      setUTCSeconds: 0
+    };
+  }
+
+  function matchDayOfWeek(token, tokens, context) {
+    var matcher = /^(Mon|Tue|Wed|Thu|Fri|Sat|Sun)u?s?n?e?s?r?s?d?a?y?$/i;
+    var key = token.match(matcher)[1].toLowerCase();
+    var dayOffset = {
+      'sun': 0,
+      'mon': 1,
+      'tue': 2,
+      'wed': 3,
+      'thu': 4,
+      'fri': 5,
+      'sat': 6
+    }[key];
+
+    if (!Number.isInteger(dayOffset)) {
+      throw 'Unrecognised day of the week: ' + dayOffset;
+    }
+
+    dayOffset = (dayOffset - context.getDay() + 7) % 7 || 7;
+    var now = context.getTime();
+    var future = new Date(now + (dayOffset * DAY));
+
+    return {
+      setUTCFullYear: future.getUTCFullYear(),
+      setUTCMonth: future.getUTCMonth(),
+      setUTCDate: future.getUTCDate(),
+      setUTCHours: 8,
+      setUTCMinutes: 0,
+      setUTCSeconds: 0
+    };
+  }
+
+  function tokenize(inputString) {
+    var plainText = inputString.replace(/[^a-z0-9+:\/\-]+/gi, ' ');
+    var sanitized = plainText.replace(/\s+/gi, ' ');
+    var tokens = sanitized.split(' ');
+    return tokens;
+  }
+
+  function dateFor(dateString, context) {
+    var tokens = tokenize(dateString);
+    var token;
+
+    var results = [];
+    while (tokens.length > 0) {
+      token = tokens.shift();
+      matchers.forEach(function(matcher) {
+        if (matcher.regex.test(token)) {
+          var result = matcher.handler(token, tokens, context);
+          if (interpretDate.debug) {
+            console.log(LOG_TABS, 'Matched', JSON.stringify(result));
+          }
+          results.push(result);
+        };
+      });
+    }
+
+    var date = new Date(context);
+    results.forEach(function(item) {
+      Object.keys(item).forEach(function(key) {
+        var value = item[key];
+        date[key](value);
+      });
     });
+
+    return date;
   }
 
-  var date = new Date(context);
-  results.forEach(function(item) {
-    Object.keys(item).forEach(function(key) {
-      var value = item[key];
-      date[key](value);
-    });
-  });
+  if (typeof module !== 'undefined') {
+    module.exports = interpretDate;
+  }
 
-  return date;
-}
-
-if (typeof module !== 'undefined') {
-  module.exports = interpretDate;
-}
+  if (typeof window !== 'undefined') {
+    window.interpretDate = interpretDate;
+  }
+})();
---
+++ web/js/time-until.js
@@ -0,0 +1,60 @@
+(function() {
+  var LOG_TABS = '     ';
+  const SECOND = 1000;
+  const MINUTE = SECOND * 60;
+  const HOUR = MINUTE * 60;
+  const DAY = HOUR * 24;
+  const WEEK = DAY * 7;
+  const MONTH = DAY * 30.417;
+  const YEAR = DAY * 365.25;
+
+  function describeTimeUntil(input) {
+    var unit, value, plural, past;
+    var ms = Math.abs(input);
+
+    if (ms < SECOND) {
+      return 'now';
+    } else if (ms < 2 * MINUTE) {
+      value = Math.floor(ms / SECOND);
+      unit = 'second';
+    } else if (ms < 2 * HOUR) {
+      value = Math.floor(ms / MINUTE);
+      unit = 'minute';
+    } else if (ms < 2 * DAY) {
+      value = Math.floor(ms / HOUR);
+      unit = 'hour';
+    } else if (ms < 3 * WEEK) {
+      value = Math.round(ms / DAY);
+      unit = 'day';
+    } else if (ms < 3 * MONTH) {
+      value = Math.round(ms / WEEK);
+      unit = 'week';
+    } else if (ms < 2 * YEAR) {
+      value = Math.round(ms / MONTH);
+      unit = 'month';
+    } else {
+      value = Math.floor(ms / YEAR);
+      unit = 'year';
+    }
+
+    plural = (value > 1) ? 's' : '';
+    past = (input < 0) ? 'in the past' : '';
+
+    return [value, unit + plural, past].join(' ').trim();
+  }
+
+  function timeUntil(date, context) {
+    context = context || new Date();
+    var diff = date.getTime() - context.getTime();
+
+    return describeTimeUntil(diff);
+  }
+
+  if (typeof module !== 'undefined') {
+    module.exports = timeUntil;
+  }
+
+  if (typeof window !== 'undefined') {
+    window.timeUntil = timeUntil;
+  }
+})();
