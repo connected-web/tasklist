d324df734eac84252844620f1899763be28f05f9
John Beech <john.beech@bbc.co.uk>
2017-02-15 10:46:12 +0000

    Change local express server to read from disk on every request

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-
 const fs = require('fs')
 const utf8 = 'utf8'
-const template = fs.readFileSync('./web/templates/template.inc.html', utf8)
-const navigation = fs.readFileSync('./web/templates/navigation.inc.html', utf8)
 
 app.get('/tasklist', function (req, res) {
+  const template = fs.readFileSync('./web/templates/template.inc.html', utf8)
+  const navigation = fs.readFileSync('./web/templates/navigation.inc.html', utf8)
+
   var output = template.replace('{{NAVIGATION}}', navigation)
   output = output.replace('{{NOW}}', Date.now())
   res.send(output)
