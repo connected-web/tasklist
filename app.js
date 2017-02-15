eb13aa115c64423b7506aecf3aeb010be21adafe
John Beech <john.beech@bbc.co.uk>
2017-02-15 08:40:47 +0000

    Sync node templates with PHP templates

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+
+const fs = require('fs')
+const utf8 = 'utf8'
+const template = fs.readFileSync('./web/templates/template.inc.html', utf8)
+const navigation = fs.readFileSync('./web/templates/navigation.inc.html', utf8)
+
+app.get('/tasklist', function (req, res) {
+  var output = template.replace('{{NAVIGATION}}', navigation)
+  output = output.replace('{{NOW}}', Date.now())
+  res.send(output)
+})
+
 app.use('/tasklist', express.static(path.join(__dirname, 'web')))
 
 const tasklist = {
--- web/index.php
+++ web/index.php
@@ -1,9 +1,9 @@
 <?php
 
-ob_start();
-require('./templates/template.inc.html');
-$output = ob_get_clean();
+$template = @file_get_contents('./templates/template.inc.html');
+$navigation = @file_get_contents('./templates/navigation.inc.html');
 
+$output = str_replace('{{NAVIGATION}}', $navigation, $template);
 $output = str_replace('{{NOW}}', time(), $output);
 
 echo $output;
