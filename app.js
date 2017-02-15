58a016426e3edbc2253965a924f601799c824889
John Beech <john.beech@bbc.co.uk>
2017-02-15 08:30:11 +0000

    Create express wrapper for hosting tasklist

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use('/tasklist', express.static(path.join(__dirname, 'web')))
+
+const tasklist = {
+  tasks: require(path.join(__dirname, 'state', 'tasklist.json'))
+}
+
+app.get('/tasklist/tasks/json', function (req, res) {
+  res.json(tasklist)
+})
+
+const port = 8000;
+app.listen(port, function () {
+  console.log(`Taklist listening on port ${port}!`)
+});
