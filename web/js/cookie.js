81dc98188af78faf335580fd3a534ec3889eca1a
John Beech <github@mkv25.net>
2017-02-16 00:25:31 +0000

    Add cookie class to javascript

---
+++ web/js/cookie.js
@@ -0,0 +1,73 @@
+(function () {
+  function readCookies(cookieString) {
+    cookieString = cookieString || document.cookie;
+    var cookiesArray = cookieString.split(';');
+    var cookiesObject = {};
+    cookiesArray.forEach(function (cookie) {
+      var keyAndValue = cookie.split('=');
+      var key = keyAndValue[0].trim();
+      var value = keyAndValue[1];
+      if (key && value) {
+        try {
+          cookiesObject[key] = JSON.parse(decodeURIComponent(value));
+        } catch (ex) {
+          cookiesObject[key] = value;
+        }
+      }
+    });
+    return cookiesObject;
+  }
+
+  function expiryString(expdays) {
+    expdays = expdays || 365;
+    var cookiesString = "";
+    var d = new Date();
+    d.setTime(d.getTime() + (expdays * 24 * 60 * 60 * 1000));
+
+    return d.toUTCString();
+  }
+
+  function writeCookies(cookies, expdays, path) {
+    path = path || '/';
+    var result = '';
+    delete cookies.expires;
+    for (var key in cookies) {
+      if (cookies.hasOwnProperty(key)) {
+        var value = cookies[key];
+        document.cookie = key + '=' + encodeURIComponent(JSON.stringify(value)) + ';' + expiryString(expdays) + ';path=' + path;
+      }
+    }
+  }
+
+  function removeItem(sKey, sPath, sDomain) {
+    document.cookie = encodeURIComponent(sKey) +
+      '=; expires=Thu, 01 Jan 1970 00:00:00 GMT' +
+      (sDomain ? '; domain=' + sDomain : '') +
+      (sPath ? '; path=' + sPath : '');
+  }
+
+  function clearAllCookies(paths) {
+    paths = paths || ['/'];
+    var cookies = readCookies();
+    for (var i = 0; i < paths.length; i++) {
+      var path = paths[i];
+      for (var key in cookies) {
+        removeItem(key, path);
+      }
+    }
+  }
+
+  var Cookies = {
+    read: readCookies,
+    write: writeCookies,
+    clear: clearAllCookies
+  };
+
+  if (typeof module !== 'undefined') {
+    module.exports = Cookies;
+  }
+
+  if (typeof window !== 'undefined') {
+    window.interpretDate = Cookies;
+  }
+})();
