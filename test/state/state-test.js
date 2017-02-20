9fa4840ee06a43cca3143b0f10537056790dfbe6
John Beech <john.beech@bbc.co.uk>
2017-02-20 23:12:23 +0000

    Test tasklist data for correctness

--- test/state/state-test.js
+++ test/state/state-test.js
@@ -5,4 +5,20 @@ describe('Tasklist Data', () => {
   it('should be structured as an array', () => {
     assert.ok(Array.isArray(state));
   });
+
+  state.forEach((task) => {
+    describe(`${task.text}`, () => {
+      it(`should have a description`, () => {
+        assert.ok(/^[A-z\s'\-,:\.!\/0-9]+$/i.test(task.text));
+      });
+
+      it(`should have a date string`, () => {
+        assert.ok(/^[A-z\s'\-,:\.!\/0-9]+$/i.test(task.dateString));
+      });
+
+      it(`should have an entry date`, () => {
+        assert.ok(Number.isFinite(task.entryDate));
+      });
+    });
+  });
 });
