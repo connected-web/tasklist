68e241e919f9131e8af0ff3a19b961d6b1d837ca
John Beech <github@mkv25.net>
2017-03-21 12:26:41 +0000

    Redirect from HTTP to HTTPS

--- web/index.php
+++ web/index.php
@@ -1,5 +1,12 @@
 <?php
 
+if($_SERVER['HTTP_HOST'] !== 'localhost' && (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off")) {
+  $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  header('HTTP/1.1 301 Moved Permanently');
+  header('Location: ' . $redirect);
+  exit();
+}
+
 $template = @file_get_contents('./templates/template.inc.html');
 $navigation = @file_get_contents('./templates/navigation.inc.html');
 
