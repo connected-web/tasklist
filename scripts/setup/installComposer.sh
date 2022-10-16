b56a4355b43317b836da69b7f7c516eeb49427ec
John Beech <johnbeech.github@mkv25.net>
2022-10-16 10:46:07 +0100

    Update composer setup scripts

--- scripts/setup/installComposer.sh
+++ scripts/setup/installComposer.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-mv composer.phar web/auth/composer.phar
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+exit $RESULT
---
+++ scripts/setup/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_ROOT="$SCRIPT_DIR/../../"
+
+$SCRIPT_DIR/installComposer.sh
+mv composer.phar $PROJECT_ROOT/web
+rm -f $PROJECT_ROOT/web/auth/composer.lock
+
+cd $PROJECT_ROOT/web/auth
+php $PROJECT_ROOT/web/composer.phar install
